0814d97fd47f332a1bd0985438c777c9f770ece5
Rafhael <rafhael.brum@gmail.com>
2025-04-01 16:55:56 -0300

    feat: add Expenses

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,23 @@
+import { Expense } from "../types/expense";
+import { writeExpenses, readExpenses } from "../utils/fileManager"
 
 
-export default function add(desc: string, amount: number) {
-    console.log(`added ${ amount } ${ desc }`);
+export default function add(description: string, amount: number) {
+    const fileData = readExpenses();
+    const dateNow = new Date();
+    let maxId = 0;
+    if (fileData.length !== 0) {
+        const ids = fileData.map(({ id }) => id);
+        maxId = Math.max(...ids);
+    }
+
+    let newExpense: Expense = {
+        id: maxId + 1,
+        description: description,
+        amount: amount,
+        date: dateNow.toISOString(),
+    }
+    fileData.push(newExpense);
+
+    writeExpenses(fileData);
 }
--- src/utils/fileManager.ts
+++ src/utils/fileManager.ts
@@ -1,20 +1,20 @@
 import fs from 'fs';
 import { Expense } from "../types/expense";
 
-function ensureExpensesFileExists(): void {
+export function ensureExpensesFileExists(): void {
     if (fs.existsSync('expenses.json')) return;
     else fs.writeFileSync('expenses.json', '[]');
 }
 
 
-function readExpenses(): Expense[] {
+export function readExpenses(): Expense[] {
     ensureExpensesFileExists();
     const data = fs.readFileSync('expenses.json', 'utf-8');
     const expenses = JSON.parse(data);
     return expenses;
 }
 
-function writeExpenses(expenses: Expense[]): void {
+export function writeExpenses(expenses: Expense[]): void {
     ensureExpensesFileExists();
     let newExpense = JSON.stringify(expenses, null, 2);
     fs.writeFileSync('expenses.json', newExpense);
